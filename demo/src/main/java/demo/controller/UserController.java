97841d3c9baac10e255815983270e5750f44be0a
UserSCP <sebastian.pardo.scp@gmail.com>
2024-09-23 20:37:21 -0300

    crud

--- demo/src/main/java/demo/controller/UserController.java
+++ demo/src/main/java/demo/controller/UserController.java
@@ -1,10 +1,16 @@
 package demo.controller;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import demo.repo.UserRepo;
 import demo.model.User;
 
@@ -18,11 +24,56 @@ public class UserController {
     public String getAllUsers(Model model) {
         try {
             List<User> users = userRepository.findAll();
-            model.addAttribute("users", users); // Pasa la lista de usuarios al modelo
-            return "users"; // Nombre de la vista Thymeleaf
+            model.addAttribute("users", users); 
+            return "users"; 
         } catch (Exception e) {
-            e.printStackTrace(); // Imprime el error en los logs para depuración
-            return "error"; // Nombre de la vista de error en caso de excepción
+            e.printStackTrace(); 
+            return "error"; 
         }
     }
+    
+   
+    
+    
+    @GetMapping("/user/{id}")
+    public String getUserById(Model model, @PathVariable long id) {
+        try {
+            Optional<User> userOptional = userRepository.findById((long) id);
+            
+            if (userOptional.isPresent()) {
+                User user = userOptional.get();
+                model.addAttribute("user", user);
+                return "user"; 
+            } else {
+                model.addAttribute("errorMessage", "Usuario no encontrado");
+                return "error"; 
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            model.addAttribute("errorMessage", "Se produjo un error al obtener el usuario");
+            return "error"; 
+        }
+    }
+    @GetMapping("/user/edit/{id}")
+    public String editUser(Model model, @PathVariable long id) {
+        Optional<User> userOptional = userRepository.findById((long) id);
+        if (userOptional.isPresent()) {
+            model.addAttribute("user", userOptional.get());
+            return "edit"; 
+        }
+        model.addAttribute("errorMessage", "Usuario no encontrado");
+        return "error"; 
+    }
+    @PostMapping("/user/delete/{id}")
+    public String deleteUser(@PathVariable long id) {
+        userRepository.deleteById((long) id);
+        return "redirect:/users"; 
+    }
+
+    @PostMapping("/user/update")
+    public String updateUser(@ModelAttribute User user) {
+        userRepository.save(user); 
+        return "redirect:/users"; 
+    }
+
 }
